4d5d855ec5a7ba6a71a441e4657784e3a877fe8f
prakash <ps8780909@gmail.com>
2025-01-27 18:32:20 +0530

    debugging

--- backend/index.js
+++ backend/index.js
@@ -13,71 +13,72 @@ const port = process.env.PORT ;
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads"); // Folder to store uploaded files
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname); // Unique filenames
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads"); // Folder to store uploaded files
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname); // Unique filenames
+//   },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
 
-app.post(
-  "/api/form",
-  upload.array("images"), 
-  [body("name", "Enter a valid name").isLength({ min: 2 })],
-  async (req, res) => {
-    try {
-      const { name, socialHandle } = req.body;
+// app.post(
+//   "/api/form",
+//   upload.array("images"), 
+//   [body("name", "Enter a valid name").isLength({ min: 2 })],
+//   async (req, res) => {
+//     try {
+//       const { name, socialHandle } = req.body;
 
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      // console.log("Uploaded files:", req.files);
+//       const errors = validationResult(req);
+//       if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//       }
+//       // console.log("Uploaded files:", req.files);
       
-      const fileData = req.files.map((file) => ({
-        path: file.path,
-        filename: file.filename,
-      }));
+//       const fileData = req.files.map((file) => ({
+//         path: file.path,
+//         filename: file.filename,
+//       }));
       
-      const newData = new Data({
-        name,
-        socialHandle,
-        images: fileData, // Save the array of uploaded images
-      });
+//       const newData = new Data({
+//         name,
+//         socialHandle,
+//         images: fileData, // Save the array of uploaded images
+//       });
 
-      // Save to database
-      const savedData = await newData.save();
+//       // Save to database
+//       const savedData = await newData.save();
 
-      res.status(201).json({
-        message: "Profile and images uploaded successfully",
-        data: savedData,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ error: "Server error: Unable to save data" });
-    }
-  }
-);
+//       res.status(201).json({
+//         message: "Profile and images uploaded successfully",
+//         data: savedData,
+//       });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send({ error: "Server error: Unable to save data" });
+//     }
+//   }
+// );
+
+
+// app.get("/api/fetchallUser", async (req, res) => {
+//   try {
+//     const user = await Data.find();
+//     res.json(user);
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send("Internal server error");
+//   }
+// });
 
 app.get("/", async (req, res) => {
   console.log("Server has been started");
-});
-
-app.get("/api/fetchallUser", async (req, res) => {
-  try {
-    const user = await Data.find();
-    res.json(user);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal server error");
-  }
 });
 
 app.listen(port, () => {
